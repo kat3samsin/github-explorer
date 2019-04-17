c08931aecd5703f728d61b840d1fac65a0f65024
Tantay, Katrina Lou S <ksamsin@netsuite.com>
2019-04-17 15:43:15 +0800

    Updates

---
+++ precache-manifest.28ca05433af84bd6ed5b2acfca0a6cf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af32253a9ea109004770",
+    "url": "/github-explorer/static/js/runtime~main.7bc3497e.js"
+  },
+  {
+    "revision": "9e11db780ae74e7b4d03",
+    "url": "/github-explorer/static/js/main.86ced8f5.chunk.js"
+  },
+  {
+    "revision": "d0b1fcfdb52efac6925e",
+    "url": "/github-explorer/static/js/2.110bd096.chunk.js"
+  },
+  {
+    "revision": "9e11db780ae74e7b4d03",
+    "url": "/github-explorer/static/css/main.ec00f8b6.chunk.css"
+  },
+  {
+    "revision": "24d8fe3bc22b92bb682bcbd8962ea478",
+    "url": "/github-explorer/index.html"
+  }
+];
